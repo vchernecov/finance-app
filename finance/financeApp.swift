1e958976a68c92e401a625dfb401cf46e693f2b4
Viktor Chernetsov <VChernetsov@alfabank.ru>
2024-08-15 10:59:17 +0200

    Initial Commit

---
+++ finance/financeApp.swift
@@ -0,0 +1,17 @@
+//
+//  financeApp.swift
+//  finance
+//
+//  Created by Viktor Chernetsov on 15.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct financeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ financeUITests/financeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  financeUITestsLaunchTests.swift
+//  financeUITests
+//
+//  Created by Viktor Chernetsov on 15.08.2024.
+//
+
+import XCTest
+
+final class financeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
